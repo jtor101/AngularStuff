5057009c042f51445ee70ada97e04da9e1585f80
John Torres <jtorres0802@gmail.com>
2019-09-24 10:18:00 -0400

    Uploaded new and updated Angular demos.

--- demo3/src/app/app.module.ts
+++ demo3/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, FooterComponent, LoginComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- modelsdemo/src/app/app.module.ts
+++ modelsdemo/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from './header/header.component';
+import { FormsModule } from "@angular/forms";
+import { FooterComponent } from './footer/footer.component';
+import { MountainComponent } from './mountain/mountain.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, FooterComponent, MountainComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
